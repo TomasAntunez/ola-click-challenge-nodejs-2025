3ebd29a9d5e50ffc2d78731d3a4c9056a60db682
Chuli <tomasantunez.dev@gmail.com>
2025-08-19 16:16:21 -0300

    chore: config sequelize with postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Logger, Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+
+    OrdersModule,
+  ],
   providers: [Logger],
 })
 export class AppModule {}
